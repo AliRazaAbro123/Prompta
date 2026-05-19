643f7e82fbfc690452083156ea0de834150fc7b8
= <=>
2026-05-19 18:20:11 +0500

    new changes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Forces Google DNS
+
 // Load environment variables
 require("dotenv").config();
 const categoryRoutes = require("./routes/categoryRoutes");

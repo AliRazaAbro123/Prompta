8715b9b199824e9c7a117ed09f31b9d62d55ee81
= <=>
2026-05-23 12:29:23 +0500

    new updates

--- backend/index.js
+++ backend/index.js
@@ -37,9 +37,4 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Server
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+module.exports = app;
